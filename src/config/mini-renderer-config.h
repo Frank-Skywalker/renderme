2f468ef132033156c21d796be45178d92b33558e
frank <frankskywalker2022@qq.com>
2022-11-23 01:03:24 +0800

    setup src structure

--- src/config/mini-renderer-config.h
+++ src/config/mini-renderer-config.h
@@ -1,3 +1,4 @@
 // the configured options and settings for mini-renderer
-#define Tutorial_VERSION_MAJOR 0
-#define Tutorial_VERSION_MINOR 0
+#define PROJECT_VERSION_MAJOR 2
+#define PROJECT_VERSION_MINOR 3
+#define USE_DOUBLE
---
+++ src/core/mini-renderer.hpp
@@ -0,0 +1,7 @@
+#include <config/mini-renderer-config.h>
+
+#ifdef USE_DOUBLE
+typedef Float double
+#else
+typedef Float float
+#endif
