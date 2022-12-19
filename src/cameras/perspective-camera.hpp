0c73374586b51689e8b22ae216926ada2f91f276
frank <frankskywalker2022@qq.com>
2022-12-19 21:44:13 +0800

    developing camera movement

--- src/cameras/perspective-camera.hpp
+++ src/cameras/perspective-camera.hpp
@@ -10,11 +10,15 @@ namespace renderme
 			Point3f position;
 			Vector3f direction;
 			Vector3f up;
+
+			Float speed{1.0f};
 		};
 
 
 		auto generate_ray() const noexcept ->Float;
 		auto imgui_config() ->void;
+		auto process_keyboard_input(Camera_Movement move, float delta_time)->void;
+		auto process_mouse_input(Float xoffset, Float yoffset)->void;
 
 		Config config;
 	};
--- src/core/camera.hpp
+++ src/core/camera.hpp
@@ -4,11 +4,22 @@
 
 namespace renderme
 {
+	enum struct Camera_Movement
+	{
+		forward,
+		backward,
+		left,
+		right
+	};
+
 	struct Camera: Only_Movable
 	{
 		virtual auto generate_ray() const noexcept ->Float = 0;
 		virtual auto imgui_config() ->void = 0;
+		virtual auto process_keyboard_input(Camera_Movement move, float delta_time)->void = 0;
+		virtual auto process_mouse_input(Float xoffset, Float yoffset)->void = 0;
 
 		Transform camera_to_world;
+		Transform world_to_camera;
 	};
 }
