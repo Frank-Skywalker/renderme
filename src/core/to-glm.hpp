93a5692a33b35c0f68a71698151fe3234c8dc37a
frank <frankskywalker2022@qq.com>
2023-01-06 20:47:01 +0800

    glm::mat4 is column major Mat is row major

--- src/core/to-glm.hpp
+++ src/core/to-glm.hpp
@@ -24,11 +24,12 @@ namespace renderme
 	template<typename T>
 	inline auto to_glm(Mat<4, 4, T> m)->glm::mat4
 	{
+		auto m_trans = m.transpose();
 		return glm::mat4(
-			m.m[0][0], m.m[0][1], m.m[0][2], m.m[0][3],
-			m.m[1][0], m.m[1][1], m.m[1][2], m.m[1][3],
-			m.m[2][0], m.m[2][1], m.m[2][2], m.m[2][3],
-			m.m[3][0], m.m[3][1], m.m[3][2], m.m[3][3]
+			m_trans.m[0][0], m_trans.m[0][1], m_trans.m[0][2], m_trans.m[0][3],
+			m_trans.m[1][0], m_trans.m[1][1], m_trans.m[1][2], m_trans.m[1][3],
+			m_trans.m[2][0], m_trans.m[2][1], m_trans.m[2][2], m_trans.m[2][3],
+			m_trans.m[3][0], m_trans.m[3][1], m_trans.m[3][2], m_trans.m[3][3]
 		);
 	}
 
@@ -45,13 +46,32 @@ namespace renderme
 
 	inline auto from_glm(glm::mat4 m)->Matrix4f
 	{
+		auto m_trans = glm::transpose(m);
 		return Matrix4f(
-			m[0][0], m[0][1], m[0][2], m[0][3],
-			m[1][0], m[1][1], m[1][2], m[1][3],
-			m[2][0], m[2][1], m[2][2], m[2][3],
-			m[3][0], m[3][1], m[3][2], m[3][3]
+			m_trans[0][0], m_trans[0][1], m_trans[0][2], m_trans[0][3],
+			m_trans[1][0], m_trans[1][1], m_trans[1][2], m_trans[1][3],
+			m_trans[2][0], m_trans[2][1], m_trans[2][2], m_trans[2][3],
+			m_trans[3][0], m_trans[3][1], m_trans[3][2], m_trans[3][3]
 		);
 	}
 
+
+	inline auto print(glm::mat4 m)->void
+	{
+		std::cout << "[" << std::endl
+			<< m[0][0] << " " << m[1][0] << " " << m[2][0] << " " << m[3][0] << std::endl
+			<< m[0][1] << " " << m[1][1] << " " << m[2][1] << " " << m[3][1] << std::endl
+			<< m[0][2] << " " << m[1][2] << " " << m[2][2] << " " << m[3][2] << std::endl
+			<< m[0][3] << " " << m[1][3] << " " << m[2][3] << " " << m[3][3] << std::endl
+			<< std::endl << "]" << std::endl;
+	}
+
+	inline auto print(float* m)->void
+	{
+		for (auto i = 0; i < 16; ++i) {
+			std::cout << m[i] << " ";
+		}
+		std::cout << std::endl;
+	}
 
 }
