163560c71bf9117cf05dde85121b7b0b681599aa
frank <frankskywalker2022@qq.com>
2023-03-12 13:52:53 +0800

    Modify .gitignore

--- src/core/config.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-// the configured options and settings for renderme
-#define PROJECT_VERSION_MAJOR 0
-#define PROJECT_VERSION_MINOR 0
-#define RENDERME_ROOT_PATH "C:/Frank/Codes/renderme"
-/* #undef USE_DOUBLE */
-/* #undef USE_CUDA */
-
-#define EPSILON 1e-8
